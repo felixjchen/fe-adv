a9b934b1f2ecf646a7e5c5ee8f104991f3423620
felixjchen <31393977+felixjchen@users.noreply.github.com>
2021-11-20 10:40:03 -0500

    auth

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import firebaseConfig from "./config";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+// https://github.com/firebase/firebaseui-web/pull/850
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const authui = new firebaseui.auth.AuthUI(auth);
+console.log({ app, auth, authui });
+const uiConfig = {
+  signInSuccessUrl: "/",
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  tosUrl: "/",
+  privacyPolicyUrl: function () {
+    window.location.assign("/");
+  },
+};
+
+firebase.auth().onAuthStateChanged((user) => {
+  console.log({ user });
+
+  if (user != null) {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  } else {
+    authui.start("#root", uiConfig);
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
