708e5df1e143c159ad22c2e6d0aa2002ff83e564
felixjchen <31393977+felixjchen@users.noreply.github.com>
2021-11-19 23:53:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Stack from "@mui/material/Stack";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 const PlayerContext = React.createContext(() => {});
 const COLORS = {
   blue: true,
